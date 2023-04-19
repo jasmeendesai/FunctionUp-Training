56c94664752bfbe167919fcb119066940b39073b
jasmeendesai <jasmeendesai2597@gmail.com>
2023-04-20 02:03:26 +0530

    written date, month and batchinfo functions

---
+++ src/util/helper.js
@@ -0,0 +1,12 @@
+const printDate= function(){
+    console.log("19")
+}
+const printMonth= function(){
+    console.log("april")
+} 
+const getBatchInfo = function(){
+    console.log("Technetium, W3D4, the topic for today is Nodejs module system")
+}
+module.exports.printDate = printDate;
+module.exports.printMonth = printMonth;
+module.exports.getBatchInfo = getBatchInfo;
